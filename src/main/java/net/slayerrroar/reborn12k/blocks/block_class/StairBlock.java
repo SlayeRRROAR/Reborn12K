294d0e38993dd969d86597916716efda36e02867
SlayeRRROAR <elmancho1@alice.it>
2021-11-11 16:25:15 +0100

    added public class for custom stairs block

---
+++ src/main/java/net/slayerrroar/reborn12k/blocks/block_class/StairBlock.java
@@ -0,0 +1,10 @@
+package net.slayerrroar.reborn12k.blocks.block_class;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StairsBlock;
+
+public class StairBlock extends StairsBlock {
+    public StairBlock(BlockState baseBlockState, Settings settings) {
+        super(baseBlockState, settings);
+    }
+}
