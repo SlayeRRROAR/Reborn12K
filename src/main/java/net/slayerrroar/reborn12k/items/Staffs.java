2db7f351705ea017e7f1ca78207b4a035b0a5484
SlayeRRROAR <elmancho1@alice.it>
2021-11-16 20:12:58 +0100

    added viridian, volcanic and windstorm staff variants

--- src/main/java/net/slayerrroar/reborn12k/items/Staffs.java
+++ src/main/java/net/slayerrroar/reborn12k/items/Staffs.java
@@ -22,6 +22,18 @@ import net.slayerrroar.reborn12k.items.item_class.weapons.sunlight.SunlightRod;
 import net.slayerrroar.reborn12k.items.item_class.weapons.sunlight.SunlightScepter;
 import net.slayerrroar.reborn12k.items.item_class.weapons.sunlight.SunlightStaff;
 import net.slayerrroar.reborn12k.items.item_class.weapons.sunlight.SunlightWand;
+import net.slayerrroar.reborn12k.items.item_class.weapons.viridian.ViridianRod;
+import net.slayerrroar.reborn12k.items.item_class.weapons.viridian.ViridianScepter;
+import net.slayerrroar.reborn12k.items.item_class.weapons.viridian.ViridianStaff;
+import net.slayerrroar.reborn12k.items.item_class.weapons.viridian.ViridianWand;
+import net.slayerrroar.reborn12k.items.item_class.weapons.volcanic.VolcanicRod;
+import net.slayerrroar.reborn12k.items.item_class.weapons.volcanic.VolcanicScepter;
+import net.slayerrroar.reborn12k.items.item_class.weapons.volcanic.VolcanicStaff;
+import net.slayerrroar.reborn12k.items.item_class.weapons.volcanic.VolcanicWand;
+import net.slayerrroar.reborn12k.items.item_class.weapons.windstorm.WindstormRod;
+import net.slayerrroar.reborn12k.items.item_class.weapons.windstorm.WindstormScepter;
+import net.slayerrroar.reborn12k.items.item_class.weapons.windstorm.WindstormStaff;
+import net.slayerrroar.reborn12k.items.item_class.weapons.windstorm.WindstormWand;
 
 public class Staffs {
 
@@ -31,10 +43,25 @@ public class Staffs {
     public static final Item MAGICAL_WOOD_STAFF = new Item(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K));
 
 
+    public static final Item WINDSTORM_WAND = new WindstormWand(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof());
+    public static final Item WINDSTORM_SCEPTER = new WindstormScepter(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
+    public static final Item WINDSTORM_ROD = new WindstormRod(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.RARE));
+    public static final Item WINDSTORM_STAFF = new WindstormStaff(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.EPIC));
+
+    public static final Item VIRIDIAN_WAND = new ViridianWand(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof());
+    public static final Item VIRIDIAN_SCEPTER = new ViridianScepter(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
+    public static final Item VIRIDIAN_ROD = new ViridianRod(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.RARE));
+    public static final Item VIRIDIAN_STAFF = new ViridianStaff(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.EPIC));
+
     public static final Item ABYSS_WAND = new AbyssWand(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof());
     public static final Item ABYSS_SCEPTER = new AbyssScepter(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
     public static final Item ABYSS_ROD = new AbyssRod(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.RARE));
     public static final Item ABYSS_STAFF = new AbyssStaff(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.EPIC));
+
+    public static final Item VOLCANIC_WAND = new VolcanicWand(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof());
+    public static final Item VOLCANIC_SCEPTER = new VolcanicScepter(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
+    public static final Item VOLCANIC_ROD = new VolcanicRod(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.RARE));
+    public static final Item VOLCANIC_STAFF = new VolcanicStaff(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.EPIC));
 
     public static final Item SUNLIGHT_WAND = new SunlightWand(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof());
     public static final Item SUNLIGHT_SCEPTER = new SunlightScepter(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).maxCount(1).fireproof().rarity(Rarity.UNCOMMON));
@@ -58,10 +85,25 @@ public class Staffs {
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "golden_rod"), GOLDEN_ROD);
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "magical_wood_staff"), MAGICAL_WOOD_STAFF);
 
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "windstorm_wand"), WINDSTORM_WAND);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "windstorm_scepter"), WINDSTORM_SCEPTER);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "windstorm_rod"), WINDSTORM_ROD);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "windstorm_staff"), WINDSTORM_STAFF);
+
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "viridian_wand"), VIRIDIAN_WAND);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "viridian_scepter"), VIRIDIAN_SCEPTER);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "viridian_rod"), VIRIDIAN_ROD);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "viridian_staff"), VIRIDIAN_STAFF);
+
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "abyss_wand"), ABYSS_WAND);
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "abyss_scepter"), ABYSS_SCEPTER);
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "abyss_rod"), ABYSS_ROD);
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "abyss_staff"), ABYSS_STAFF);
+
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "volcanic_wand"), VOLCANIC_WAND);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "volcanic_scepter"), VOLCANIC_SCEPTER);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "volcanic_rod"), VOLCANIC_ROD);
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "volcanic_staff"), VOLCANIC_STAFF);
 
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "sunlight_wand"), SUNLIGHT_WAND);
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "sunlight_scepter"), SUNLIGHT_SCEPTER);
---
+++ src/main/java/net/slayerrroar/reborn12k/items/item_class/weapons/windstorm/WindstormRod.java
@@ -0,0 +1,62 @@
+package net.slayerrroar.reborn12k.items.item_class.weapons.windstorm;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class WindstormRod extends Item {
+
+    public WindstormRod(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+
+        tooltip.add(new TranslatableText("item.reborn12k.windstorm.tooltip"));
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+
+        world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_EVOKER_PREPARE_SUMMON, SoundCategory.PLAYERS, 0.25f, 1.0f);
+        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*30, 2));
+        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 20*30, 3));
+        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 20*30, 0));
+
+        for (Entity e : world.getOtherEntities(playerEntity, Box.of(playerEntity.getPos(), 20, 20, 20))) {
+            if (e instanceof PlayerEntity playerEntities) {
+                if (playerEntity.distanceTo(playerEntities) < 20) {
+                    playerEntities.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*30, 2));
+                    playerEntities.addStatusEffect(new StatusEffectInstance(StatusEffects.HASTE, 20*30, 3));
+                    playerEntities.addStatusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 20*30, 0));
+                }
+            }
+        }
+
+        playerEntity.getItemCooldownManager().set(this, 20*60);
+
+        return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+
+    }
+}
