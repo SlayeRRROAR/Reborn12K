e41e692ead62c00250fbd5ff142972ca55fccd4e
SlayeRRROAR <elmancho1@alice.it>
2021-11-25 14:33:51 +0100

    moved file

--- src/main/java/net/slayerrroar/reborn12k/util/Fuels.java
+++ src/main/java/net/slayerrroar/reborn12k/util/Fuels.java
@@ -1,4 +1,4 @@
-package net.slayerrroar.reborn12k.fuel;
+package net.slayerrroar.reborn12k.util;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.slayerrroar.reborn12k.blocks.BaseBlocks;
