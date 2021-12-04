fc14e5390f68e524397b9f896757f7bd675fcf68
SlayeRRROAR <elmancho1@alice.it>
2021-12-04 14:57:29 +0100

    added new charm

--- src/main/java/net/slayerrroar/reborn12k/items/Charms.java
+++ src/main/java/net/slayerrroar/reborn12k/items/Charms.java
@@ -8,14 +8,17 @@ import net.minecraft.util.registry.Registry;
 import net.slayerrroar.reborn12k.Reborn12K;
 import net.slayerrroar.reborn12k.items.item_class.charms.MagnetCharm;
 import net.slayerrroar.reborn12k.items.item_class.charms.MirrorCharm;
+import net.slayerrroar.reborn12k.items.item_class.charms.NatureCharm;
 
 public class Charms {
 
-    public static final Item MAGIC_MIRROR = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).fireproof().maxDamage(16));
-    public static final Item ICE_MIRROR = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).fireproof().maxDamage(16));
-    public static final Item CELL_PHONE = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).fireproof().maxDamage(16));
+    public static final Item MAGIC_MIRROR = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).maxDamage(16));
+    public static final Item ICE_MIRROR = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).maxDamage(16));
+    public static final Item CELL_PHONE = new MirrorCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).maxDamage(16));
 
-    public static final Item MAGNET = new MagnetCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1).fireproof());
+    public static final Item MAGNET = new MagnetCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1));
+
+    public static final Item YGGDRASILL_BRANCH = new NatureCharm(new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K).rarity(Rarity.EPIC).maxCount(1));
 
     public static void register() {
 
@@ -25,6 +28,7 @@ public class Charms {
 
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "magnet"), MAGNET);
 
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "yggdrasill_branch"), YGGDRASILL_BRANCH);
     }
 
 }
