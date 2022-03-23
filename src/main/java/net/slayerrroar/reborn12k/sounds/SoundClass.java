9564b8e40fee6d26f34ec477268d8e8226951725
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-03-23 11:53:52 +0100

    added sound Devil Trigger

--- src/main/java/net/slayerrroar/reborn12k/sounds/SoundClass.java
+++ src/main/java/net/slayerrroar/reborn12k/sounds/SoundClass.java
@@ -8,6 +8,7 @@ import net.slayerrroar.reborn12k.Reborn12K;
 public class SoundClass {
 
     public static final SoundEvent Z115 = registerSound("z115");
+    public static final SoundEvent DEVIL_TRIGGER = registerSound("devil_trigger");
 
     private static SoundEvent registerSound(String string) {
         Identifier id = new Identifier(Reborn12K.MOD_ID, string);
