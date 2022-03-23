f6cf4fd745c2d4dd597dc1e78ef103a2f9fc1d53
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-03-23 12:00:35 +0100

    added slimy tree

--- src/main/java/net/slayerrroar/reborn12k/blocks/block_class/CustomSaplingBlock.java
+++ src/main/java/net/slayerrroar/reborn12k/blocks/block_class/CustomSaplingBlock.java
@@ -3,9 +3,9 @@ package net.slayerrroar.reborn12k.blocks.block_class;
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.sapling.SaplingGenerator;
 
-public class CherrySaplingBlock extends SaplingBlock {
+public class CustomSaplingBlock extends SaplingBlock {
 
-    public CherrySaplingBlock(SaplingGenerator generator, Settings settings) {
+    public CustomSaplingBlock(SaplingGenerator generator, Settings settings) {
         super(generator, settings);
     }
 }
