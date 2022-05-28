e9efb45792084e67f4b60f4f471de536efa62204
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-05-28 13:42:53 +0200

    changed to allow different sounds

--- src/main/java/net/slayerrroar/reborn12k/villagers/CustomVillagers.java
+++ src/main/java/net/slayerrroar/reborn12k/villagers/CustomVillagers.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableSet;
 import net.fabricmc.fabric.mixin.object.builder.PointOfInterestTypeAccessor;
 import net.fabricmc.fabric.mixin.object.builder.VillagerProfessionAccessor;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -16,16 +15,22 @@ import net.slayerrroar.reborn12k.blocks.AdvancedBlocks;
 public class CustomVillagers {
 
     public static final PointOfInterestType DEALER_POI = registerPOI("dealerpoi", AdvancedBlocks.MARKET_TABLE);
-    public static final VillagerProfession DEALER = registerProfession("dealer", DEALER_POI);
+    public static final VillagerProfession DEALER = registerDealer("dealer", DEALER_POI);
 
-    public static final PointOfInterestType BLACKSMITH_POI = registerPOI("blacksmithpoi", Blocks.ANVIL);
-    public static final VillagerProfession BLACKSMITH = registerProfession("blacksmith", BLACKSMITH_POI);
+    public static final PointOfInterestType BLACKSMITH_POI = registerPOI("blacksmithpoi", AdvancedBlocks.ALLOY_FURNACE);
+    public static final VillagerProfession BLACKSMITH = registerBlacksmith("blacksmith", BLACKSMITH_POI);
 
 
-    public static VillagerProfession registerProfession(String name, PointOfInterestType type) {
+    public static VillagerProfession registerBlacksmith(String name, PointOfInterestType type) {
         return Registry.register(Registry.VILLAGER_PROFESSION, new Identifier(Reborn12K.MOD_ID, name),
                 VillagerProfessionAccessor.create(name, type, ImmutableSet.of(), ImmutableSet.of(),
                         SoundEvents.ENTITY_VILLAGER_WORK_TOOLSMITH));
+    }
+
+    public static VillagerProfession registerDealer(String name, PointOfInterestType type) {
+        return Registry.register(Registry.VILLAGER_PROFESSION, new Identifier(Reborn12K.MOD_ID, name),
+                VillagerProfessionAccessor.create(name, type, ImmutableSet.of(), ImmutableSet.of(),
+                        SoundEvents.ENTITY_VILLAGER_WORK_LIBRARIAN));
     }
 
     public static PointOfInterestType registerPOI(String name, Block block) {
