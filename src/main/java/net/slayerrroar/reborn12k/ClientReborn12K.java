0d5085f3ff05e36f59e7e2a295d3489050911061
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-05-28 13:41:55 +0200

    added screen registry

--- src/main/java/net/slayerrroar/reborn12k/ClientReborn12K.java
+++ src/main/java/net/slayerrroar/reborn12k/ClientReborn12K.java
@@ -3,9 +3,13 @@ package net.slayerrroar.reborn12k;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.slayerrroar.reborn12k.screen.AlloyFurnaceScreen;
+import net.slayerrroar.reborn12k.screen.CustomScreenHandlers;
 import net.slayerrroar.reborn12k.util.CustomModelPredicateProvider;
 import net.slayerrroar.reborn12k.util.RebornRenderHelper;
 
+@SuppressWarnings("deprecation")
 @Environment(EnvType.CLIENT)
 
 public class ClientReborn12K implements ClientModInitializer {
@@ -15,6 +19,8 @@ public class ClientReborn12K implements ClientModInitializer {
 
        RebornRenderHelper.setRenderLayers();
        CustomModelPredicateProvider.registerCustomModels();
+
+       ScreenRegistry.register(CustomScreenHandlers.ALLOY_FURNACE_SCREEN_HANDLER, AlloyFurnaceScreen::new);
 
     }
 }
