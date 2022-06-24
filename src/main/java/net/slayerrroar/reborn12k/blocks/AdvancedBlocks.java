a2bb972cf271d59078fba8d53a6cebb144f409fe
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-06-24 23:32:42 +0200

    renamed star generator to fancy lamp

--- src/main/java/net/slayerrroar/reborn12k/blocks/AdvancedBlocks.java
+++ src/main/java/net/slayerrroar/reborn12k/blocks/AdvancedBlocks.java
@@ -18,7 +18,7 @@ import net.slayerrroar.reborn12k.util.Reborn12KItemGroup;
 public class AdvancedBlocks {
 
     public static final Block MARKET_TABLE = new MarketBlock(FabricBlockSettings.copy(Blocks.CRAFTING_TABLE).nonOpaque());
-    public static final Block STAR_GENERATOR = new StarGeneratorBlock(FabricBlockSettings.copy(Blocks.STONE).sounds(BlockSoundGroup.STONE).hardness(5.0f).resistance(6.0f).nonOpaque().luminance(blockstate ->30));
+    public static final Block FANCY_LAMP = new StarGeneratorBlock(FabricBlockSettings.copy(Blocks.STONE).sounds(BlockSoundGroup.STONE).hardness(5.0f).resistance(6.0f).nonOpaque().luminance(blockstate ->30));
 
     public static final Block ALLOY_FURNACE = new AlloyFurnaceBlock(FabricBlockSettings.copy(Blocks.STONE));
 
@@ -27,7 +27,7 @@ public class AdvancedBlocks {
         //BLOCK
 
         Registry.register(Registry.BLOCK, new Identifier(Reborn12K.MOD_ID, "market_table"), MARKET_TABLE);
-        Registry.register(Registry.BLOCK, new Identifier(Reborn12K.MOD_ID, "star_generator"), STAR_GENERATOR);
+        Registry.register(Registry.BLOCK, new Identifier(Reborn12K.MOD_ID, "fancy_lamp"), FANCY_LAMP);
 
         Registry.register(Registry.BLOCK, new Identifier(Reborn12K.MOD_ID, "alloy_furnace"), ALLOY_FURNACE);
 
@@ -36,7 +36,7 @@ public class AdvancedBlocks {
         //ITEM
 
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "market_table"), new BlockItem(MARKET_TABLE, new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K)));
-        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "star_generator"), new BlockItem(STAR_GENERATOR, new FabricItemSettings().rarity(Rarity.RARE).group(Reborn12KItemGroup.REBORN12K)));
+        Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "fancy_lamp"), new BlockItem(FANCY_LAMP, new FabricItemSettings().rarity(Rarity.RARE).group(Reborn12KItemGroup.REBORN12K)));
 
         Registry.register(Registry.ITEM, new Identifier(Reborn12K.MOD_ID, "alloy_furnace"), new BlockItem(ALLOY_FURNACE, new FabricItemSettings().group(Reborn12KItemGroup.REBORN12K)));
 
