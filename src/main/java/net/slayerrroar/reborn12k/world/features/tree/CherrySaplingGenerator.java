7b5fdb2c7abd8d4ac2b871c485ca6749e75c824c
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-06-24 23:49:03 +0200

    fixed tree and tree generation

--- src/main/java/net/slayerrroar/reborn12k/world/features/tree/CherrySaplingGenerator.java
+++ src/main/java/net/slayerrroar/reborn12k/world/features/tree/CherrySaplingGenerator.java
@@ -1,12 +1,11 @@
 package net.slayerrroar.reborn12k.world.features.tree;
 
 import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.slayerrroar.reborn12k.world.features.RebornConfiguredFeatures;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Random;
 
 public class CherrySaplingGenerator extends SaplingGenerator {
 
--- src/main/java/net/slayerrroar/reborn12k/world/generation/RebornTreeGen.java
+++ src/main/java/net/slayerrroar/reborn12k/world/generation/RebornTreeGen.java
@@ -3,6 +3,7 @@ package net.slayerrroar.reborn12k.world.generation;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 import net.slayerrroar.reborn12k.world.features.RebornPlacedFeatures;
 
@@ -10,10 +11,10 @@ import net.slayerrroar.reborn12k.world.features.RebornPlacedFeatures;
 public class RebornTreeGen {
 
     public static void generateTrees() {
-        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.FOREST),
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
                 GenerationStep.Feature.VEGETAL_DECORATION, RebornPlacedFeatures.CHERRY_PLACED.getKey().get());
 
-        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.SWAMP),
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.SWAMP),
                 GenerationStep.Feature.VEGETAL_DECORATION, RebornPlacedFeatures.SLIMY_PLACED.getKey().get());
 
     }
