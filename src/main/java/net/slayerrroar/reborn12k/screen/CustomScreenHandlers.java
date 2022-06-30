c9d93d1456b274c2d08d65fe6cf9c29c6641a90b
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-06-30 19:04:20 +0200

    added new screenhandler, fixed file location

--- src/main/java/net/slayerrroar/reborn12k/screen/CustomScreenHandlers.java
+++ src/main/java/net/slayerrroar/reborn12k/screen/CustomScreenHandlers.java
@@ -4,14 +4,23 @@ import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.slayerrroar.reborn12k.Reborn12K;
+import net.slayerrroar.reborn12k.screen.alloy_furnace.AlloyFurnaceScreenHandler;
+import net.slayerrroar.reborn12k.screen.runic_artifact.RunicArtifactScreenHandler;
 
 @SuppressWarnings("ALL")
 public class CustomScreenHandlers {
 
     public static ScreenHandlerType<AlloyFurnaceScreenHandler> ALLOY_FURNACE_SCREEN_HANDLER;
 
+    public static ScreenHandlerType<RunicArtifactScreenHandler> RUNIC_ARTIFACT_SCREEN_HANDLER;
+
     public static void register() {
+
         ALLOY_FURNACE_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier(Reborn12K.MOD_ID, "alloy_furnace"),
                 AlloyFurnaceScreenHandler::new);
+
+        RUNIC_ARTIFACT_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier(Reborn12K.MOD_ID, "runic_artifact"),
+                RunicArtifactScreenHandler::new);
+
     }
 }
