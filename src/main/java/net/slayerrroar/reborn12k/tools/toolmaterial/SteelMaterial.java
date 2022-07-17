e1782a4e205baca19197d4248a442d13e7dff766
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-07-17 11:52:03 +0200

    renamed steel from carbon steel

---
+++ src/main/java/net/slayerrroar/reborn12k/tools/toolmaterial/SteelMaterial.java
@@ -0,0 +1,41 @@
+package net.slayerrroar.reborn12k.tools.toolmaterial;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.slayerrroar.reborn12k.items.BaseItems;
+
+public class SteelMaterial implements ToolMaterial{
+
+    public static final SteelMaterial INSTANCE = new SteelMaterial();
+
+    @Override
+    public float getAttackDamage() {
+        return 4.0f;
+    }
+
+    @Override
+    public int getDurability() {
+        return 1562;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 5;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 3;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 9.0f;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(BaseItems.STEEL_INGOT);
+    }
+    
+}
