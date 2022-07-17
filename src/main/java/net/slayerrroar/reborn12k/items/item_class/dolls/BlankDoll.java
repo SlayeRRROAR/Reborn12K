2a6dca83e944dbb9f01c5bfcc795a0e0f6e6362e
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2022-07-17 11:27:49 +0200

    removed all magic related stuff + advancements

--- src/main/java/net/slayerrroar/reborn12k/items/item_class/dolls/BlankDoll.java
+++
@@ -1,9 +0,0 @@
-package net.slayerrroar.reborn12k.items.item_class.dolls;
-
-import net.minecraft.item.Item;
-
-public class BlankDoll extends Item {
-    public BlankDoll(Settings settings) {
-        super(settings);
-    }
-}
