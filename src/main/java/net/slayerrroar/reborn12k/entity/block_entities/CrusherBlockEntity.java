14793fb2590f3e33e179fae54c5b81a1b75b1992
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-03-05 17:00:38 +0100

    new stuff

---
+++ src/main/java/net/slayerrroar/reborn12k/entity/block_entities/CrusherBlockEntity.java
@@ -0,0 +1,228 @@
+package net.slayerrroar.reborn12k.entity.block_entities;
+
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.slayerrroar.reborn12k.blocks.custom_blocks.CrusherBlock;
+import net.slayerrroar.reborn12k.entity.CustomBlockEntities;
+import net.slayerrroar.reborn12k.recipe.recipe_types.CrusherRecipe;
+import net.slayerrroar.reborn12k.screen.crusher.CrusherScreenHandler;
+import net.slayerrroar.reborn12k.util.ImplementedInventory;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class CrusherBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
+    private final DefaultedList<ItemStack> inventory =
+            DefaultedList.ofSize(3, ItemStack.EMPTY);
+
+    protected final PropertyDelegate propertyDelegate;
+    private int progress= 0;
+    private int maxProgress = 199;
+    private int fuelTime= 0;
+    private int maxFuelTime= 0;
+
+    public CrusherBlockEntity(BlockPos pos, BlockState state) {
+        super(CustomBlockEntities.CRUSHER, pos, state);
+        this.propertyDelegate = new PropertyDelegate() {
+            @Override
+            public int get(int index) {
+                return switch (index) {
+                    case 0 -> CrusherBlockEntity.this.progress;
+                    case 1 -> CrusherBlockEntity.this.maxProgress;
+                    case 2 -> CrusherBlockEntity.this.fuelTime;
+                    case 3 -> CrusherBlockEntity.this.maxFuelTime;
+                    default -> 0;
+                };
+            }
+
+
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0 -> CrusherBlockEntity.this.progress = value;
+                    case 1 -> CrusherBlockEntity.this.maxProgress = value;
+                    case 2 -> CrusherBlockEntity.this.fuelTime = value;
+                    case 3 -> CrusherBlockEntity.this.maxFuelTime = value;
+                }
+            }
+
+            public int size() {
+                return 4;
+            }
+        };
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return this.inventory;
+    }
+
+    @Override
+    public Text getDisplayName() {
+        return Text.translatable("block.reborn12k.crusher");
+    }
+
+    @Nullable
+    @Override
+    public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
+        return new CrusherScreenHandler(syncId, inv, this, this.propertyDelegate);
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt) {
+        Inventories.writeNbt(nbt, inventory);
+        super.writeNbt(nbt);
+        nbt.putInt("crusher.progress", progress);
+        nbt.putInt("crusher.fueltime", fuelTime);
+        nbt.putInt("crusher.maxfueltime", maxFuelTime);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        Inventories.readNbt(nbt,inventory);
+        super.readNbt(nbt);
+        progress = nbt.getInt("crusher.progress");
+        fuelTime = nbt.getInt("crusher.fueltime");
+        maxFuelTime = nbt.getInt("crusher.maxfueltime");
+    }
+
+    private void consumeFuel() {
+        if(!getStack(0).isEmpty()) {
+            this.fuelTime = FuelRegistry.INSTANCE.get(this.removeStack(0, 1).getItem());
+            this.maxFuelTime = this.fuelTime;
+        }
+    }
+
+    public static void tick(World world, BlockPos blockPos, BlockState state, CrusherBlockEntity entity) {
+        if(world.isClient()) {
+            return;
+        }
+        if(isConsumingFuel(entity)) {
+            entity.fuelTime--;
+        }
+        if(hasRecipe(entity)) {
+            if(hasFuelInFuelSlot(entity) && !isConsumingFuel(entity)) {
+                entity.consumeFuel();
+            }
+            if(isConsumingFuel(entity)) {
+                entity.progress++;
+                markDirty(world, blockPos, state);
+                if(entity.progress >= entity.maxProgress) {
+                    craftItem(entity);
+                }
+            }
+        } else {
+            entity.resetProgress();
+            markDirty(world, blockPos, state);
+        }
+    }
+
+    private static boolean hasFuelInFuelSlot(CrusherBlockEntity entity) {
+        return !entity.getStack(0).isEmpty();
+    }
+
+    private static boolean isConsumingFuel(CrusherBlockEntity entity) {
+        return entity.fuelTime > 0;
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
+    }
+
+    private static void craftItem(CrusherBlockEntity entity) {
+        SimpleInventory inventory = new SimpleInventory(entity.size());
+        for (int i = 0; i < entity.size(); i++) {
+            inventory.setStack(i, entity.getStack(i));
+        }
+
+        Optional<CrusherRecipe> recipe = entity.getWorld().getRecipeManager()
+                .getFirstMatch(CrusherRecipe.Type.INSTANCE, inventory, entity.getWorld());
+
+        if(hasRecipe(entity)) {
+
+            entity.removeStack(1,1);
+
+            entity.setStack(2, new ItemStack(recipe.get().getOutput().getItem(),
+                    entity.getStack(2).getCount() + 1));
+
+            entity.resetProgress();
+        }
+    }
+
+    private static boolean hasRecipe(CrusherBlockEntity entity) {
+        SimpleInventory inventory = new SimpleInventory(entity.size());
+        for (int i = 0; i < entity.size(); i++) {
+            inventory.setStack(i, entity.getStack(i));
+        }
+
+        Optional<CrusherRecipe> match = entity.getWorld().getRecipeManager()
+                .getFirstMatch(CrusherRecipe.Type.INSTANCE, inventory, entity.getWorld());
+
+        return match.isPresent() && canInsertAmountIntoOutputSlot(inventory)
+                && canInsertItemIntoOutputSlot(inventory, match.get().getOutput().getItem());
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleInventory inventory, Item output) {
+        return inventory.getStack(2).getItem() == output || inventory.getStack(2).isEmpty();
+    }
+
+    private static boolean canInsertAmountIntoOutputSlot(SimpleInventory inventory) {
+        return inventory.getStack(2).getMaxCount() > inventory.getStack(2).getCount();
+    }
+
+    @Override
+    public boolean canInsert(int slot, ItemStack stack, @Nullable Direction side) {
+        Direction localDir = this.getWorld().getBlockState(this.pos).get(CrusherBlock.FACING);
+
+        if(side == Direction.DOWN) {
+            return false;
+        }
+
+        return switch (localDir) {
+            default ->
+                    side.getOpposite() == Direction.DOWN && slot == 1 ||
+                            side.getOpposite() == Direction.NORTH && slot == 0 ||
+                            side.getOpposite() == Direction.EAST && slot == 0 ||
+                            side.getOpposite() == Direction.WEST && slot == 0;
+            case EAST ->
+                    side.getOpposite() == Direction.DOWN && slot == 1 ||
+                            side.rotateYClockwise() == Direction.NORTH && slot == 0 ||
+                            side.rotateYClockwise() == Direction.EAST && slot == 0 ||
+                            side.rotateYClockwise() == Direction.WEST && slot == 0;
+            case SOUTH ->
+                    side.getOpposite() == Direction.DOWN && slot == 1 ||
+                            side == Direction.NORTH && slot == 0 ||
+                            side == Direction.EAST && slot == 0 ||
+                            side == Direction.WEST && slot == 0;
+            case WEST ->
+                    side.getOpposite() == Direction.DOWN && slot == 1 ||
+                            side.rotateYCounterclockwise() == Direction.NORTH && slot == 0 ||
+                            side.rotateYCounterclockwise() == Direction.EAST && slot == 0 ||
+                            side.rotateYCounterclockwise() == Direction.WEST && slot == 0;
+        };
+    }
+
+    @Override
+    public boolean canExtract(int slot, ItemStack stack, Direction side) {
+
+        if(side == Direction.DOWN) {
+            return slot == 2;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/net/slayerrroar/reborn12k/screen/crusher/CrusherScreenHandler.java
@@ -0,0 +1,110 @@
+package net.slayerrroar.reborn12k.screen.crusher;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.slayerrroar.reborn12k.screen.RebornScreenRegistry;
+import net.slayerrroar.reborn12k.screen.slots.CustomBucketSlot;
+import net.slayerrroar.reborn12k.screen.slots.CustomFuelSlot;
+import net.slayerrroar.reborn12k.screen.slots.CustomResultSlot;
+
+public class CrusherScreenHandler extends ScreenHandler {
+    private final Inventory inventory;
+    private final PropertyDelegate propertyDelegate;
+
+    public CrusherScreenHandler(int syncId, PlayerInventory inventory) {
+        this(syncId, inventory, new SimpleInventory(3), new ArrayPropertyDelegate(4));
+    }
+
+    public CrusherScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate delegate) {
+        super(RebornScreenRegistry.CRUSHER_SCREEN_HANDLER, syncId);
+        checkSize(inventory, 3);
+        this.inventory = inventory;
+        inventory.onOpen(playerInventory.player);
+        this.propertyDelegate = delegate;
+
+        this.addSlot(new CustomFuelSlot(inventory, 0, 56, 53));
+        this.addSlot(new Slot(inventory, 1, 56, 17));
+        this.addSlot(new CustomResultSlot(inventory, 2, 116, 35));
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+
+        addProperties(delegate);
+
+    }
+
+    public boolean isCrafting() {
+        return propertyDelegate.get(0) > 0;
+    }
+
+    public boolean hasFuel() {
+        return propertyDelegate.get(2) > 0;
+    }
+
+    public int getScaledProgress() {
+        int progress = this.propertyDelegate.get(0);
+        int maxProgress = this.propertyDelegate.get(1);
+        int progressArrowSize = 24;
+
+        return maxProgress != 0 && progress != 0 ? progress * progressArrowSize / maxProgress : 0;
+    }
+
+    public int getScaledFuelProgress() {
+        int fuelProgress = this.propertyDelegate.get(2);
+        int maxFuelProgress = this.propertyDelegate.get(3);
+        int fuelProgressSize = 14;
+
+        return maxFuelProgress != 0 ? (int)(((float)fuelProgress / (float)maxFuelProgress) * fuelProgressSize) : 0;
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int invSlot) {
+        ItemStack newStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(invSlot);
+        if (slot != null && slot.hasStack()) {
+            ItemStack originalStack = slot.getStack();
+            newStack = originalStack.copy();
+            if(invSlot < this.inventory.size()) {
+                if(!this.insertItem(originalStack,this.inventory.size(), this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (originalStack.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+        }
+        return newStack;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return this.inventory.canPlayerUse(player);
+    }
+
+    private void addPlayerInventory(PlayerInventory playerInventory) {
+        for (int i = 0; i < 3; ++i) {
+            for (int l = 0; l < 9; ++l) {
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(PlayerInventory playerInventory) {
+        for (int i = 0; i < 9; ++i) {
+            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
+        }
+    }
+
+}
