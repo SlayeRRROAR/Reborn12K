ab6fd903d44f9a5211c167d54bde894cb917134a
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-09 20:27:34 +0200

    fixed my ocd :)

--- src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/shoes/TravelerShoes.java
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/shoes/TravelerShoes.java
@@ -30,9 +30,8 @@ public class TravelerShoes extends TrinketItem implements Trinket {
 
     @Override
     public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
-        tooltip.add(Text.translatable("item.reborn12k.traveler_shoes.tooltip1"));
-        tooltip.add(Text.translatable("item.reborn12k.traveler_shoes.tooltip2"));
         tooltip.add(Text.translatable("item.reborn12k.shoes.tooltip"));
+        tooltip.add(Text.translatable("item.reborn12k.rare.tooltip"));
     }
 
 }
