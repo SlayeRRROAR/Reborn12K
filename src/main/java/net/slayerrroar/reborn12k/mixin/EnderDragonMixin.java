a0a2538436eb44b32192fdd43e0887edf5a2b2d8
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-09 18:14:01 +0200

    added mixin to allow subsequient killed dragons to spawn other blocks (atm is an additional dragon egg)

---
+++ src/main/java/net/slayerrroar/reborn12k/mixin/EnderDragonMixin.java
@@ -0,0 +1,67 @@
+package net.slayerrroar.reborn12k.mixin;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.gen.feature.EndPortalFeature;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.UUID;
+
+@SuppressWarnings("JavaDoc")
+
+@Mixin(EnderDragonFight.class)
+public abstract class EnderDragonMixin {
+
+    @Final
+    @Shadow
+    private ServerBossBar bossBar;
+
+    @Shadow
+    private UUID dragonUuid;
+
+    @Shadow
+    protected abstract void generateEndPortal(boolean previouslyKilled);
+
+    @Shadow
+    protected abstract void generateNewEndGateway();
+
+    @Final
+    @Shadow
+    private ServerWorld world;
+
+    @Shadow
+    private boolean previouslyKilled;
+
+    @Shadow
+    private boolean dragonKilled;
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void dragonKilled(EnderDragonEntity dragon) {
+        if (dragon.getUuid().equals(this.dragonUuid)) {
+            this.bossBar.setPercent(0.0f);
+            this.bossBar.setVisible(false);
+            this.generateEndPortal(true);
+            this.generateNewEndGateway();
+            if(!this.previouslyKilled) {
+                this.world.setBlockState(this.world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, EndPortalFeature.ORIGIN), Blocks.DRAGON_EGG.getDefaultState());
+            }
+            if(this.previouslyKilled) {
+                this.world.setBlockState(this.world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, EndPortalFeature.ORIGIN), Blocks.DRAGON_EGG.getDefaultState());
+            }
+            this.previouslyKilled = true;
+            this.dragonKilled = true;
+        }
+    }
+
+}
