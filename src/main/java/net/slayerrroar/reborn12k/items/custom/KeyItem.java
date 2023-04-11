9cd8b2a5023129e1c8c7fbc66c7a8406443a98ec
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-11 10:21:45 +0200

    strongboxes can now be opened with keys

---
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/KeyItem.java
@@ -0,0 +1,21 @@
+package net.slayerrroar.reborn12k.items.custom;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class KeyItem extends Item {
+    public KeyItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.reborn12k.key.tooltip"));
+    }
+}
