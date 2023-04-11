9a82b41918ade05c691f4cf94742e3705776a3d1
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-11 12:56:45 +0200

    reworked sword crit chance to work with lucky charms

--- src/main/java/net/slayerrroar/reborn12k/mixin/SwordCritMixin.java
+++ src/main/java/net/slayerrroar/reborn12k/mixin/SwordCritMixin.java
@@ -1,5 +1,6 @@
 package net.slayerrroar.reborn12k.mixin;
 
+import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -7,6 +8,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.slayerrroar.reborn12k.items.ItemTrinkets;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,22 +16,48 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
+@SuppressWarnings("OptionalGetWithoutIsPresent")
+
 @Mixin(SwordItem.class)
 public class SwordCritMixin {
 
-    @Inject(method = "postHit", at = @At(value = "RETURN"))
+    private boolean hasVagueLuck(LivingEntity entity) {
+        return TrinketsApi.getTrinketComponent(entity).get().isEquipped(ItemTrinkets.VAGUE_LUCKY_CHARM);
+    }
+    private boolean hasMinorLuck(LivingEntity entity) {
+        return TrinketsApi.getTrinketComponent(entity).get().isEquipped(ItemTrinkets.MINOR_LUCKY_CHARM);
+    }
+    private boolean hasIndelibleLuck(LivingEntity entity) {
+        return TrinketsApi.getTrinketComponent(entity).get().isEquipped(ItemTrinkets.INDELIBLE_LUCKY_CHARM);
+    }
+    private boolean hasGreaterLuck(LivingEntity entity) {
+        return TrinketsApi.getTrinketComponent(entity).get().isEquipped(ItemTrinkets.GREATER_LUCKY_CHARM);
+    }
+
+    @Inject(method = "postHit", at = @At(value = "HEAD"))
     public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker, CallbackInfoReturnable<Boolean> cir) {
+        float damage = (float) attacker.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+        assert target != null;
 
         Random rand = new Random();
         int upperbound = 101;
         int int_random = rand.nextInt(upperbound);
 
-        if (int_random < 6 && target != null) {
-            float damage = (float) attacker.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+        int vagueLuck = hasVagueLuck(attacker) ? 0 : 5;
+        int minorLuck = hasMinorLuck(attacker) ? 0 : 10;
+        int indelibleLuck = hasIndelibleLuck(attacker) ? 0 : 15;
+        int greaterLuck = hasGreaterLuck(attacker) ? 0 : 20;
+
+        int critChance = int_random + vagueLuck + minorLuck + indelibleLuck + greaterLuck;
+
+        if(critChance > 90) {
             float critBonus = 1.5F;
             target.damage(DamageSource.GENERIC, (damage * critBonus));
             target.world.playSound(null, target.getX(), target.getY(), target.getZ(),
                     SoundEvents.ENTITY_PLAYER_ATTACK_CRIT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+        }
+        if(critChance <= 90) {
+            target.damage(DamageSource.GENERIC, (damage));
         }
     }
 }
