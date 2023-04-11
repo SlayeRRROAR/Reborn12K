20159d0a513fdc1b08d62849c5db612eff5577db
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-11 10:20:43 +0200

    added missing ash

--- src/main/java/net/slayerrroar/reborn12k/util/StrippableUtil.java
+++ src/main/java/net/slayerrroar/reborn12k/util/StrippableUtil.java
@@ -6,6 +6,9 @@ import net.slayerrroar.reborn12k.blocks.BlockGenerics;
 public class StrippableUtil {
 
     public static void register() {
+        StrippableBlockRegistry.register(BlockGenerics.ASH_LOG, BlockGenerics.STRIPPED_ASH_LOG);
+        StrippableBlockRegistry.register(BlockGenerics.ASH_WOOD, BlockGenerics.STRIPPED_ASH_WOOD);
+
         StrippableBlockRegistry.register(BlockGenerics.CHERRY_LOG, BlockGenerics.STRIPPED_CHERRY_LOG);
         StrippableBlockRegistry.register(BlockGenerics.CHERRY_WOOD, BlockGenerics.STRIPPED_CHERRY_WOOD);
     }
