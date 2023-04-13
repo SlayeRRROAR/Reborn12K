4ed488ed74147133186e35869fa85e261fb1d672
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-13 15:07:13 +0200

    optimized imports

--- src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/necklace/LightPendant.java
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/necklace/LightPendant.java
@@ -1,21 +1,14 @@
 package net.slayerrroar.reborn12k.items.custom.trinkets.necklace;
 
-import com.google.common.collect.Multimap;
-import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.Trinket;
 import dev.emi.trinkets.api.TrinketItem;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.UUID;
 
 public class LightPendant extends TrinketItem implements Trinket {
 
