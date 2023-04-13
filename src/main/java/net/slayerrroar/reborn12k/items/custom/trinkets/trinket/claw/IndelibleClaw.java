d838a1e18047a4587ff62bdfa7e374f2f0eb3fbe
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-13 14:00:14 +0200

    changed values

--- src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/trinket/claw/IndelibleClaw.java
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/trinket/claw/IndelibleClaw.java
@@ -24,7 +24,7 @@ public class IndelibleClaw extends TrinketItem implements Trinket {
 
     public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
         var modifiers = super.getModifiers(stack, slot, entity, uuid);
-        modifiers.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(uuid, "reborn12k:attack_damage", 0.2, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+        modifiers.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(uuid, "reborn12k:attack_damage", 0.3, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
         return modifiers;
     }
 
