a3f64a38c77ea9725dfa69752da539178afa3317
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-04-25 18:56:42 +0200

    removed temp tooltip

--- src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/trinket/band/IndelibleBand.java
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/trinkets/trinket/band/IndelibleBand.java
@@ -48,7 +48,6 @@ public class IndelibleBand extends TrinketItem implements Trinket {
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.translatable("item.reborn12k.trinket.tooltip"));
-        tooltip.add(Text.translatable("item.reborn12k.common.tooltip"));
         tooltip.add(Text.translatable("item.reborn12k.epic.tooltip"));
     }
 
