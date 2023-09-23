4edd990afd36d5150b4396c1b61d72ee4a7445e6
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-09-23 20:34:57 +0200

    added new enchantment

--- src/main/java/net/slayerrroar/reborn12k/enchantments/RebornEnchantments.java
+++ src/main/java/net/slayerrroar/reborn12k/enchantments/RebornEnchantments.java
@@ -9,6 +9,7 @@ import net.minecraft.util.Identifier;
 import net.slayerrroar.reborn12k.Reborn12K;
 import net.slayerrroar.reborn12k.enchantments.enchant.armor.MagicProtectionEnchantment;
 import net.slayerrroar.reborn12k.enchantments.enchant.bow.WoundingEnchantment;
+import net.slayerrroar.reborn12k.enchantments.enchant.weapon.BloodLustEnchantment;
 import net.slayerrroar.reborn12k.enchantments.enchant.weapon.FrostBiteEnchantment;
 import net.slayerrroar.reborn12k.enchantments.enchant.weapon.KoDeathEnchantment;
 import net.slayerrroar.reborn12k.enchantments.enchant.weapon.ToxicEdgeEnchantment;
@@ -17,10 +18,11 @@ public class RebornEnchantments {
 
     public static final Enchantment MAGIC_PROTECTION = new MagicProtectionEnchantment(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.ARMOR, new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET});
 
-    public static final Enchantment KISS_OF_DEATH = new KoDeathEnchantment(Enchantment.Rarity.VERY_RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
-
     public static final Enchantment TOXIC_EDGE = new ToxicEdgeEnchantment(Enchantment.Rarity.RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     public static final Enchantment FROST_BITE = new FrostBiteEnchantment(Enchantment.Rarity.RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+
+    public static final Enchantment BLOODLUST = new BloodLustEnchantment(Enchantment.Rarity.VERY_RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+    public static final Enchantment KISS_OF_DEATH = new KoDeathEnchantment(Enchantment.Rarity.VERY_RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
 
     public static final Enchantment VENOM = new WoundingEnchantment(Enchantment.Rarity.RARE, EnchantmentTarget.BOW, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     public static final Enchantment WOUNDING = new WoundingEnchantment(Enchantment.Rarity.RARE, EnchantmentTarget.BOW, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
@@ -31,9 +33,11 @@ public class RebornEnchantments {
 
         Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "magic_protection"), MAGIC_PROTECTION);
 
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "kiss_of_death"), KISS_OF_DEATH);
         Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "toxic_edge"), TOXIC_EDGE);
         Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "frost_bite"), FROST_BITE);
+
+        Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "bloodlust"), BLOODLUST);
+        Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "kiss_of_death"), KISS_OF_DEATH);
 
         Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "venom"), VENOM);
         Registry.register(Registries.ENCHANTMENT, new Identifier(Reborn12K.MOD_ID, "wounding"), WOUNDING);
---
+++ src/main/java/net/slayerrroar/reborn12k/enchantments/enchant/weapon/BloodLustEnchantment.java
@@ -0,0 +1,51 @@
+package net.slayerrroar.reborn12k.enchantments.enchant.weapon;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.slayerrroar.reborn12k.enchantments.RebornEnchantments;
+
+public class BloodLustEnchantment extends Enchantment {
+    public BloodLustEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
+        super(weight, type, slotTypes);
+    }
+
+    @Override
+    public boolean isAvailableForEnchantedBookOffer() {
+        return true;
+    }
+
+    @Override
+    protected boolean canAccept(Enchantment other) {
+        return super.canAccept(other) && other != Enchantments.SHARPNESS && other != Enchantments.SMITE && other != Enchantments.BANE_OF_ARTHROPODS&& other != RebornEnchantments.KISS_OF_DEATH;
+    }
+
+    public boolean isTreasure() {
+        return true;
+    }
+
+    public int getMinPower(int level) {
+        return level * 25;
+    }
+
+    public int getMaxPower(int level) {
+        return this.getMinPower(level) + 50;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 2;
+    }
+
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+        if (user.getHealth() < user.getMaxHealth() / 2) {
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20*5, level, false, false));
+        }
+    }
+}
