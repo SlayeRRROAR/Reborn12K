733b17b9b40cdfc07a483befb9c05398f264fc81
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-10-20 10:58:54 +0200

    fixed inventory position

--- src/main/java/net/slayerrroar/reborn12k/screen/manufactory/ManufactoryScreenHandler.java
+++ src/main/java/net/slayerrroar/reborn12k/screen/manufactory/ManufactoryScreenHandler.java
@@ -134,7 +134,7 @@ public class ManufactoryScreenHandler extends ScreenHandler {
     private void addPlayerInventory(PlayerInventory playerInventory) {
         for (int i = 0; i < 3; ++i) {
             for (int l = 0; l < 9; ++l) {
-                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 84 + i * 18));
             }
         }
     }
