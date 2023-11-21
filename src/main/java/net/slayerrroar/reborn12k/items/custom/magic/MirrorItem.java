f732e22b5bb600298b6e81cb92c7319bf547b030
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2023-11-21 17:20:05 +0100

    now stores coordinates on nbt

--- src/main/java/net/slayerrroar/reborn12k/items/custom/magic/MirrorItem.java
+++ src/main/java/net/slayerrroar/reborn12k/items/custom/magic/MirrorItem.java
@@ -1,11 +1,15 @@
 package net.slayerrroar.reborn12k.items.custom.magic;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
@@ -17,26 +21,49 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.slayerrroar.reborn12k.util.InventoryInteractionUtil;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
+@SuppressWarnings("ConstantConditions")
 public class MirrorItem extends Item {
     public MirrorItem(Settings settings) {
         super(settings);
     }
 
-    static BlockPos teleportPos = null;
+    private void setPositionAndUpdate(ServerPlayerEntity player, BlockPos pos) {
+        World world = player.getWorld();
+        boolean isPosEastFree = (!world.getBlockState(pos.east()).isIn(BlockTags.BEDS) && world.getBlockState(pos.east().up()).isOf(Blocks.AIR));
+        boolean isPosSouthFree = (!world.getBlockState(pos.south()).isIn(BlockTags.BEDS) && world.getBlockState(pos.south().up()).isOf(Blocks.AIR));
+        boolean isPosWestFree = (!world.getBlockState(pos.west()).isIn(BlockTags.BEDS) && world.getBlockState(pos.west().up()).isOf(Blocks.AIR));
+        boolean isPosNorthFree = (!world.getBlockState(pos.north()).isIn(BlockTags.BEDS) && world.getBlockState(pos.north().up()).isOf(Blocks.AIR));
 
-    public void setPositionAndUpdate(ServerPlayerEntity player, BlockPos teleportPos) {
-        player.teleport(teleportPos.getX() + 0.5f, teleportPos.getY() + 0.6F, teleportPos.getZ() + 0.5f);
-        player.fallDistance = 0;
+        if (isPosEastFree) {
+            player.fallDistance = 0;
+            player.teleport(pos.east().getX() + 0.5f, pos.getY() + 0.063f, pos.east().getZ() + 0.5f);
+        } else if (isPosSouthFree) {
+            player.fallDistance = 0;
+            player.teleport(pos.south().getX() + 0.5f, pos.getY() + 0.063f, pos.south().getZ() + 0.5f);
+        } else if (isPosWestFree) {
+            player.fallDistance = 0;
+            player.teleport(pos.west().getX() + 0.5f, pos.getY() + 0.063f, pos.west().getZ() + 0.5f);
+        } else if (isPosNorthFree) {
+            player.fallDistance = 0;
+            player.teleport(pos.north().getX() + 0.5f, pos.getY() + 0.063f, pos.north().getZ() + 0.5f);
+        } else {
+            player.fallDistance = 0;
+            player.teleport(pos.getX() + 0.5f, pos.getY() + 0.563, pos.getZ() + 0.5f);
+        }
     }
 
     private void attemptTeleport(World world, PlayerEntity player) {
         ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+        ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
 
-        if (teleportPos == null) {
+        if (getTeleportPos(stack.getOrCreateNbt()) == null) {
             player.sendMessage(Text.translatable("item.reborn12k.magic_mirror.tooltip1"), true);
         } else {
-            setPositionAndUpdate(serverPlayer, teleportPos);
+            setPositionAndUpdate(serverPlayer, getTeleportPos(stack.getOrCreateNbt()));
             world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 0.25f, 0.25f);
             player.getStackInHand(Hand.MAIN_HAND).damage(1, world.random, serverPlayer);
             player.getItemCooldownManager().set(this, 20);
@@ -44,19 +71,34 @@ public class MirrorItem extends Item {
         }
     }
 
+    private void writeNbt(BlockPos pos, NbtCompound nbt) {
+        nbt.put("TeleportPos", NbtHelper.fromBlockPos(pos));
+    }
+
+    @Nullable
+    private static BlockPos getTeleportPos(NbtCompound nbt) {
+        if (nbt.contains("TeleportPos")) {
+            return NbtHelper.toBlockPos(nbt.getCompound("TeleportPos"));
+        }
+        return null;
+    }
+
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         PlayerEntity player = context.getPlayer();
+        ItemStack stack = context.getStack();
 
         assert player != null;
         BlockState block = world.getBlockState(context.getBlockPos());
         BlockPos pos = context.getBlockPos();
 
         if (player.isSneaking() && block.isIn(BlockTags.BEDS)) {
-            teleportPos = pos;
+            if (getTeleportPos(stack.getNbt()) != null) {
+                stack.setNbt(new NbtCompound());
+            }
+            writeNbt(pos, stack.getOrCreateNbt());
             player.sendMessage(Text.translatable("item.reborn12k.magic_mirror.tooltip3"), true);
-
             return ActionResult.SUCCESS;
         } else {
             return ActionResult.PASS;
@@ -92,4 +134,12 @@ public class MirrorItem extends Item {
         return new TypedActionResult<>(ActionResult.FAIL, player.getStackInHand(hand));
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (stack.hasNbt()) {
+            tooltip.add(Text.translatable("item.reborn12k.mirror.tooltip1"));
+        } else {
+            tooltip.add(Text.translatable("item.reborn12k.mirror.tooltip2"));
+        }
+    }
 }
